837884082afb50e129976d224b1d78161e63364b
Robin <saygram@gmail.com>
2023-11-24 00:29:24 +0100

    make script working-directory independent

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 BUILD_TYPE="${BUILD_TYPE:-Release}"
 
 conan profile detect 2> /dev/null || true
-conan install -of build --output-folder=build --build=missing --settings=build_type="$BUILD_TYPE" .
+conan install -of "$SCRIPT_DIR/build" --build=missing --settings=build_type="$BUILD_TYPE" "$SCRIPT_DIR"
 
-meson setup --native-file build/conan_meson_native.ini build
-meson compile -C build
+meson setup --native-file "$SCRIPT_DIR/build/conan_meson_native.ini" "$SCRIPT_DIR/build" "$SCRIPT_DIR"
+meson compile -C "$SCRIPT_DIR/build"
