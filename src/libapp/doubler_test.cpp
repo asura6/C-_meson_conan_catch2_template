e1921389b4969da0eee57472b4e45a79f0a49769
Robin <saygram@gmail.com>
2023-11-24 01:40:07 +0100

    address sanitizer

--- src/libapp/doubler_test.cpp
+++ src/libapp/doubler_test.cpp
@@ -1,5 +1,7 @@
 #include "catch2/catch_all.hpp"
 #include <string_view>
+#include <cstdlib>
+#include <ctime>
 
 #include "doubler.hpp"
 
@@ -24,4 +26,14 @@ TEST_CASE("string types", "[doubleValue]")
     const std::string_view val = "hello";
     const auto doubledVal = doubleValue(val);
     REQUIRE(doubledVal == "hellohello");
+}
+
+TEST_CASE("sanitizer example")
+{
+    std::srand(std::time(nullptr));
+    const auto randval = std::rand();
+    auto i = (int *)malloc(sizeof(int *));
+    *i = randval;
+    free(i);
+    REQUIRE(*i == randval);
 }
